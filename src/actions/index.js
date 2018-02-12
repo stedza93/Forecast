16b4702e954d6d3b61dd6aecfa91dc492733535b
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-12 15:42:03 +0100

    Ponedeljak posao forecast

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_KEY = "8d71b2d1b288a4e7fb933991ea8e6e42";
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
