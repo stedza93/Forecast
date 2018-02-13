e096cbf689790d35537a19d2dd8a438fcd9fa577
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-13 16:04:24 +0100

    pokusati ubaciti hendler

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ export const FETCH_WEATHER = "FETCH_WEATHER";
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
+  request.then(data => console.log(data));
   return {
     type: FETCH_WEATHER,
     payload: request
