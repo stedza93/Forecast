efe8a171c3a6fcc4880b5978b9f268fd3c1f8f7c
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-14 13:46:05 +0100

    ubaciti google maps i pokusati midlwear-om rijesiti gresku nepostojeceg grada

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,11 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city}`;
+  const url = `${ROOT_URL}&q=${city}&units=metric`;
   const request = axios.get(url);
-  request.then(data => console.log(data));
+  /*request.then(data => {
+    if
+  });*/
   return {
     type: FETCH_WEATHER,
     payload: request
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,13 +3,24 @@ import { connect } from "react-redux";
 import Chart from "../components/chart";
 class WeatherList extends Component {
   renderWeather(cityData) {
+    if (cityData === undefined) {
+      return [];
+    }
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     return (
       <tr key={cityData.city.id}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="#d12929" />
+          <Chart data={temps} color="#d12929" units="°C" />
+        </td>
+        <td>
+          <Chart data={pressures} color="#abc6b5" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="#4286f4" units="%" />
         </td>
       </tr>
     );
